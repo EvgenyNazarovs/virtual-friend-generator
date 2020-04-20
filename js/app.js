c095d19cd1facea805ef2e3c854df61157c1f633
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-20 08:21:26 +0100

    can add friends

--- js/app.js
+++ js/app.js
@@ -3,38 +3,65 @@ document.addEventListener('DOMContentLoaded', () => {
   newItemForm.addEventListener('submit', handleNewItemFormSubmit);
 
   const deleteAllButton = document.querySelector('#delete-all');
-  deleteAllButtom.addEventListener('click', handleDeleteAllClick);
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
 })
 
 const handleNewItemFormSubmit = function (event) {
   event.preventDefault();
 
-  const recordCollectionItem = createRecordCollectionItem(event.target);
-  const recordCollection = document.querySelector('#record-collection');
-  recordCollection.appendChild(recordCollectionItem);
+  const virtualFriend = createVirtualFriend(event.target);
+  const virtualFriendsAll = document.querySelector('#virtual-friends');
+  virtualFriendsAll.appendChild(virtualFriend);
 
   event.target.reset();
 }
 
-const createRecordCollectionItem = function (form) {
-  const recordCollectionItem = document.createElement('li');
-  recordCollectionItem.classList.add('record-collection-item');
+const createVirtualFriend = function (form) {
+  const virtualFriend = document.createElement('li');
+  virtualFriend.style.backgroundColor = generateColour(friendColours);
+  virtualFriend.classList.add('virtual-friend-item');
 
-  const title = document.createElement('h3');
-  title.textContent = form.title.value;
-  recordCollectionItem.appendChild(title);
+  const name = document.createElement('h3');
+  name.textContent = form.name.value;
+  virtualFriend.appendChild(name);
 
-  const artist = document.createElement('h3');
-  artist.textContent = form.artist.value;
-  recordCollectionItem.appendChild(artist);
 
-  const genre = document.createElement('h4');
-  genre.textContent = form.genre.value;
-  recordCollectionItem.appendChild(genre);
+  const profession = document.createElement('h4');
+  profession.textContent = form.profession.value;
+  virtualFriend.appendChild(profession);
 
-  const type = document.createElement('h4');
-  type.textContent = form.type.value;
-  recordCollectionItem.appendChild(type);
+  const look = document.createElement('h5');
+  look.textContent = form.look.value;
+  virtualFriend.appendChild(look);
 
-  return recordCollectionItem
+  const greeting = document.createElement('h4');
+  greeting.textContent = generateGreeting(friendGreetings, form.likes.value);
+  virtualFriend.appendChild(greeting)
+
+  return virtualFriend
+}
+
+const generateGreeting = function(greetings, interest) {
+  const phrase = greetings[Math.floor(Math.random()*greetings.length)];
+  if (phrase === greetings[3]) {return `"${phrase} ${interest}."`};
+  return `"${phrase} ${interest}?"`
+}
+
+const friendGreetings = ['How about we grab some beers and chat about',
+                         'Heeey! What\'s your take on',
+                         'Good to see you! Thoughts on',
+                         'Got a minute? Let me tell you about']
+
+const generateColour = function(colours){
+  return colours[Math.floor(Math.random()*colours.length)]
+}
+
+const friendColours = ['#FF4242',
+                       '#FB62F6',
+                       '#645DD7',
+                       '#B3FFFC']
+
+const handleDeleteAllClick = function (event) {
+  const virtualFriends = document.querySelector('#virtual-friends');
+  virtualFriends.innerHTML = ""
 }
